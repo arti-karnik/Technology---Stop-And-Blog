913247abcb0a1db147925c0b48962bd4fe322ae8
Arti <arti.iosdev@gmail.com>
2021-04-08 22:34:18 -0700

    update config

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,14 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
+ /* process.env.DB_NAME,
   process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.DB_PASSWORD*/
   {
-   /* HOST: "us-cdbr-east-03.cleardb.com",
+    HOST: "us-cdbr-east-03.cleardb.com",
     USER: "b4dc0ce011ccbb",
     PASSWORD: "5347da50",
-    DB: "heroku_e2f9d8952254755",*/
+    DB: "heroku_e2f9d8952254755",
     host: 'localhost',
     dialect: 'mysql',
     port: 3306,
