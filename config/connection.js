649987dc495e4b80f9becedd9fc5678dd2519777
Arti <arti.iosdev@gmail.com>
2021-04-08 23:05:36 -0700

    add dburl in config

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,22 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
+let sequelize;
+if (process.env.DATABASE_URL) {
+    sequelize = new Sequelize(process.env.DATABASE_URL);
+} else {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    });
+}
+module.exports = sequelize;
+/*
 const sequelize = new Sequelize(
- /* process.env.DB_NAME,
+  process.env.DB_NAME,
   process.env.DB_USER,
-  process.env.DB_PASSWORD*/
+  process.env.DB_PASSWORD
   {
     HOST: "us-cdbr-east-03.cleardb.com",
     USER: "b4dc0ce011ccbb",
@@ -13,6 +25,7 @@ const sequelize = new Sequelize(
     dialect: 'mysql',
     port: 3306,
   }
-);
+)
 
-module.exports = sequelize;
+module.exports = sequelize;*/
+
